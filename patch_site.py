06479f9c5805a700c665aa9fe053f371c03933bd
zephcompany <157452403+zephcompany@users.noreply.github.com>
2026-09-07 03:23:54 -0300

    Atualiza cache da correcao de scroll

--- patch_site.py
+++ patch_site.py
@@ -3,7 +3,7 @@ import re
 
 p=Path('index.html')
 s=p.read_text(encoding='utf-8')
-ver='20260907-0319'
+ver='20260907-0323'
 
 # quebra cache do CSS/JS premium
 s=re.sub(r'<link rel="stylesheet" href="enhancements\.css\?v=[^"]+">\n?','',s)
